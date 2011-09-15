609554d917d4285a2da1df5b92fac0aecffc9649
indrek <indrek.koue@gmail.com>
2011-09-16 00:44:12 +0300

    ver 0.9

--- src/com/inc/im/serptracker/AboutActivity.java
+++ src/com/inc/im/serptracker/AboutActivity.java
@@ -1,8 +1,20 @@
 package com.inc.im.serptracker;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,13 +28,72 @@ public class AboutActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_layout);
 
-		bindInfoText("Version 0.9 released!\n\n"
-				+ "Version 0.9 adds whole new user interface. \n\n"
-				+ "1.6 version of android is not supported anymore due the lack of UI elements and functions\n\n"
-				+ "NOTE: If you are migrating from version 0.85 or lower, please reset your data under settings > delete all data");
+		// TODO: async downlod
+		if (internetConnectionExists())
+			bindInfoText(manageDownload("http://www.thedroidproject.com/_app/SERPTracker/apptext.txt"));
+		else
+			bindInfoText("ERROR: no internet connection exists");
+
+		// bindInfoText("Version 0.9 released!\n\n"
+		// + "Version 0.9 adds whole new user interface. \n\n"
+		// +
+		// "1.6 version of android is not supported anymore due the lack of UI elements and functions\n\n"
+		// +
+		// "NOTE: If you are migrating from version 0.85 or lower, please reset your data under settings > delete all data");
+
 		bindSendEmailToDevButton();
 		bindBackButton();
 
+		Toast.makeText(
+				getBaseContext(),
+				"Please send any questions and bug reports to my email by clicking \"send email button\"",
+				Toast.LENGTH_LONG).show();
+
+	}
+
+	public boolean internetConnectionExists() {
+
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public String manageDownload(String url) {
+
+		String result = null;
+		try {
+			StringBuffer sb = new StringBuffer("");
+
+			HttpGet request = new HttpGet();
+			request.setURI(URI.create(url));
+
+			HttpResponse response = new DefaultHttpClient().execute(request);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+
+			String line = "";
+			String NL = System.getProperty("line.separator");
+
+			while ((line = in.readLine()) != null)
+				sb.append(line + NL);
+
+			in.close();
+
+			result = sb.toString();
+
+		} catch (Exception e) {
+			Log.e("MY", e.toString());
+		}
+
+		return result;
 	}
 
 	private void bindBackButton() {
@@ -47,10 +118,12 @@ public class AboutActivity extends Activity {
 
 						Intent intent = new Intent(Intent.ACTION_SEND);
 						intent.setType("text/plain");
+
 						intent.putExtra(Intent.EXTRA_EMAIL,
 								new String[] { "indrek.koue@gmail.com" });
 
 						intent.putExtra(Intent.EXTRA_SUBJECT, "SERP Tracker");
+
 						try {
 							startActivity(Intent.createChooser(intent,
 									"Send mail"));
