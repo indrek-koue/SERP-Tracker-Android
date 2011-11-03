137ddc9c21a00118bea5fdedccdb890905fd35df
INDREK <indrek.koue@gmail.com>
2011-11-03 21:50:17 +0200

    main list adapter

--- src/com/inc/im/serptracker/adapters/MainActivityListAdapter.java
+++ src/com/inc/im/serptracker/adapters/MainActivityListAdapter.java
@@ -1,11 +1,13 @@
 package com.inc.im.serptracker.adapters;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.inc.im.serptracker.R;
 import com.inc.im.serptracker.data.Keyword;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,14 +51,14 @@ public class MainActivityListAdapter extends BaseAdapter {
 
 		View v = convertView;
 
-		if (v == null) {
-			// convertView = new
+		// if (v == null) {
+		// convertView = new
 
-			LayoutInflater inf = (LayoutInflater) con
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inf = (LayoutInflater) con
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-			v = inf.inflate(R.layout.main_activity_listview_item, null);
-		}
+		v = inf.inflate(R.layout.main_activity_listview_item, null);
+		// }
 
 		Keyword k = input.get(position);
 
@@ -67,48 +69,65 @@ public class MainActivityListAdapter extends BaseAdapter {
 		String keyword = k.keyword;
 		String newRank = Integer.toString(k.newRank);
 		String oldRank = Integer.toString(k.oldRank);
+		int rankChange = k.oldRank - k.newRank;
 
 		tvRankOld.setTextColor(R.color.dred);
 
 		ImageView iv = (ImageView) v.findViewById(R.id.imageView1);
 
-		if (k.newRank > k.oldRank) {
+		// debugg
+//		k.newRank = new Random().nextInt(100);
+//		k.oldRank = new Random().nextInt(100);
+
+		Log.d("MY", k.keyword + ":" + k.newRank + " " + k.oldRank);
+		// k.oldRank != 0 is for when comes from edit page or just entered
+		// keyword first run
+		if (k.newRank > k.oldRank && k.oldRank != 0) {
+			// Log.d("MY", "iv.setBackgroundResource");
 			iv.setBackgroundResource(R.drawable.down);
 			tvRankOld.setVisibility(View.VISIBLE);
 			tvRankOld.setTextColor(con.getResources().getColor(R.color.dred));
 		} else if (k.newRank < k.oldRank) {
+
+			// Log.d("MY", "iv.setBackgroundResource");
 			iv.setBackgroundResource(R.drawable.up);
 			tvRankOld.setVisibility(View.VISIBLE);
 			tvRankOld.setTextColor(con.getResources().getColor(R.color.green));
 		} else {
+
+			// Log.d("MY", "iv.setVisibility(View.GONE)");
 			iv.setVisibility(View.GONE);
 			tvRankOld.setVisibility(View.GONE);
 		}
 
 		tvKeyword.setText(keyword);
 		tvRank.setText(newRank);
-		tvRankOld.setText(oldRank);
+		tvRankOld.setText(Math.abs(rankChange)+"");
 
 		// special cases
 		// 0 - empty field in DB - new
 		// -1 - not ranked in top 100
 		// -2 - error getting data
 
+		// #SPECIAL CASE #1
 		if (k.newRank == 0) {
 			if (k.oldRank == 0) {
 
 				// just added
 				tvRank.setVisibility(View.GONE);
+				// Log.d("MY", "iv.setVisibility(View.GONE)");
 				iv.setVisibility(View.GONE);
 				tvRankOld.setVisibility(View.GONE);
 			} else {
 				// has been used before (has oldrank) and started from
 				// mainscreen first time
 				tvRank.setText(oldRank);
+				// Log.d("MY", "iv.setVisibility(View.GONE)");
 				iv.setVisibility(View.GONE);
 				tvRankOld.setVisibility(View.GONE);
 
 				if (k.oldRank == -1) {
+					// Log.d("MY", "iv.setVisibility(View.GONE)");
 					iv.setVisibility(View.GONE);
 					tvRankOld.setVisibility(View.GONE);
 					tvRank.setText("-");
@@ -116,6 +135,7 @@ public class MainActivityListAdapter extends BaseAdapter {
 
 				if (k.oldRank == -2) {
 					// hide image + old rank
+					// Log.d("MY", "iv.setVisibility(View.GONE)");
 					iv.setVisibility(View.GONE);
 					tvRankOld.setVisibility(View.GONE);
 
@@ -128,14 +148,20 @@ public class MainActivityListAdapter extends BaseAdapter {
 
 		}
 
+		// #SPECIAL CASE #2
+		// replace -1 with "-"
 		if (k.newRank == -1) {
+			// Log.d("MY", "iv.setVisibility(View.GONE)");
 			iv.setVisibility(View.GONE);
 			tvRankOld.setVisibility(View.GONE);
 			tvRank.setText("-");
 		}
 
+		// #SPECIAL CASE #3
+		// replace -2 with "error"
 		if (k.newRank == -2) {
 			// hide image + old rank
+			// Log.d("MY", "iv.setVisibility(View.GONE)");
 			iv.setVisibility(View.GONE);
 			tvRankOld.setVisibility(View.GONE);
 
