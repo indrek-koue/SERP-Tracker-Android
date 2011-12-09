e071a8496eebdfa434c912f0ce6b7740685f4ac8
Indrek Koue <indrek.koue@gmail.com>
2011-12-09 16:30:00 +0200

    refactoring

--- src/com/inc/im/serptracker/util/MainActivityHelper.java
+++ src/com/inc/im/serptracker/util/MainActivityHelper.java
@@ -11,90 +11,95 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import com.flurry.android.FlurryAgent;
 import com.inc.im.serptracker.AboutActivity;
 import com.inc.im.serptracker.InsertWebsiteActivity;
+import com.inc.im.serptracker.MainActivity;
 import com.inc.im.serptracker.ManageWebsitesActivity;
 import com.inc.im.serptracker.PreferencesActivity;
 import com.inc.im.serptracker.R;
 import com.inc.im.serptracker.adapters.DbAdapter;
 import com.inc.im.serptracker.data.Keyword;
 import com.inc.im.serptracker.data.UserProfile;
+import com.inc.im.serptracker.data.access.AsyncDownloader;
 
 public class MainActivityHelper {
 
-//	public static ArrayList<String> bindResultListView(Activity a,
-//			ListView listView, ArrayList<Keyword> keywords) {
-//
-//		ArrayList<String> keywordsToBind = new ArrayList<String>();
-//
-//		if (keywords != null) {
-//
-//			for (Keyword k : keywords) {
-//
-//				// 0 - empty field in DB - new
-//				// -1 - not ranked in top 100
-//				// -2 - error getting data
-//
-//				if ((k.newRank == -1 && k.oldRank == 0)
-//						|| (k.newRank == 0 && k.oldRank == -1)
-//						|| k.newRank == -1) {
-//					// not ranked
-//					keywordsToBind.add(k.keyword + " ["
-//							+ a.getString(R.string.not_ranked) + "]");
-//				} else if (k.newRank == -2) {
-//					// error
-//					keywordsToBind.add(k.keyword + " ["
-//							+ a.getString(R.string.error_please_try_again)
-//							+ "]");
-//				} else if (k.newRank == 0 && k.oldRank == 0) {
-//					// new keyword
-//					keywordsToBind.add(k.keyword);
-//				} else {
-//
-//					// keyword has a rank - is it new or old?
-//					int valueChange = k.oldRank - k.newRank;
-//					String ready = "";
-//
-//					if (valueChange != 0 && k.newRank != 0) {
-//
-//						// keyword first run
-//						if (k.oldRank == 0) {
-//							ready = String.format("%s [ %d ]", k.keyword,
-//									k.newRank);
-//						} else {
-//							// show with value change
-//							String sign = valueChange > 0 ? "+" : "";
-//							ready = String.format("%s [ %d ] %s%d", k.keyword,
-//									k.newRank, sign, valueChange);
-//						}
-//
-//					} else {
-//						// value change is 0 - no point to show it with +/-
-//						ready = String.format("%s [ %d ]", k.keyword, k.oldRank);
-//					}
-//
-//					keywordsToBind.add(ready);
-//					// keywordsToBind.add(k.value + " [" + k.rank + "] ");
-//				}
-//			}
-//
-//			listView.setAdapter(new ArrayAdapter<String>(a,
-//					R.layout.main_activity_listview_item, R.id.textView1,
-//					keywordsToBind));
-//
-//		}
-//
-//		return keywordsToBind;
-//
-//	}
+	// public static ArrayList<String> bindResultListView(Activity a,
+	// ListView listView, ArrayList<Keyword> keywords) {
+	//
+	// ArrayList<String> keywordsToBind = new ArrayList<String>();
+	//
+	// if (keywords != null) {
+	//
+	// for (Keyword k : keywords) {
+	//
+	// // 0 - empty field in DB - new
+	// // -1 - not ranked in top 100
+	// // -2 - error getting data
+	//
+	// if ((k.newRank == -1 && k.oldRank == 0)
+	// || (k.newRank == 0 && k.oldRank == -1)
+	// || k.newRank == -1) {
+	// // not ranked
+	// keywordsToBind.add(k.keyword + " ["
+	// + a.getString(R.string.not_ranked) + "]");
+	// } else if (k.newRank == -2) {
+	// // error
+	// keywordsToBind.add(k.keyword + " ["
+	// + a.getString(R.string.error_please_try_again)
+	// + "]");
+	// } else if (k.newRank == 0 && k.oldRank == 0) {
+	// // new keyword
+	// keywordsToBind.add(k.keyword);
+	// } else {
+	//
+	// // keyword has a rank - is it new or old?
+	// int valueChange = k.oldRank - k.newRank;
+	// String ready = "";
+	//
+	// if (valueChange != 0 && k.newRank != 0) {
+	//
+	// // keyword first run
+	// if (k.oldRank == 0) {
+	// ready = String.format("%s [ %d ]", k.keyword,
+	// k.newRank);
+	// } else {
+	// // show with value change
+	// String sign = valueChange > 0 ? "+" : "";
+	// ready = String.format("%s [ %d ] %s%d", k.keyword,
+	// k.newRank, sign, valueChange);
+	// }
+	//
+	// } else {
+	// // value change is 0 - no point to show it with +/-
+	// ready = String.format("%s [ %d ]", k.keyword, k.oldRank);
+	// }
+	//
+	// keywordsToBind.add(ready);
+	// // keywordsToBind.add(k.value + " [" + k.rank + "] ");
+	// }
+	// }
+	//
+	// listView.setAdapter(new ArrayAdapter<String>(a,
+	// R.layout.main_activity_listview_item, R.id.textView1,
+	// keywordsToBind));
+	//
+	// }
+	//
+	// return keywordsToBind;
+	//
+	// }
 
 	public static ArrayList<Keyword> findKeywordsByName(
 			ArrayList<UserProfile> data, int spinnerSelectedValueNr) {
@@ -248,6 +253,113 @@ public class MainActivityHelper {
 		}
 
 		return displayed;
+	}
+
+	public static String getSpinnerSelectedValue(Activity a) {
+
+		Object o = ((Spinner) a.findViewById(R.id.spinner_profile))
+				.getSelectedItem();
+		if (o != null)
+			return o.toString();
+		else
+			return "";
+
+	}
+
+	public static int getSpinnerSelectedIndex(Activity a) {
+
+		return ((Spinner) a.findViewById(R.id.spinner_profile))
+				.getSelectedItemPosition();
+
+	}
+
+	public static void initSpinner(final Activity a) {
+
+		final ArrayList<UserProfile> data = new DbAdapter(a).loadAllProfiles();
+		Spinner spinner = (Spinner) a.findViewById(R.id.spinner_profile);
+
+		ArrayList<String> spinnerValues = new ArrayList<String>();
+		spinnerValues.add(a.getString(R.string.spinner_default_selection));
+
+		// generate spinner values
+		if (data != null)
+			for (UserProfile u : data)
+				spinnerValues.add(u.url + "[" + u.keywords.size() + "]");
+
+		spinner.setAdapter(new ArrayAdapter<String>(a.getBaseContext(),
+				R.layout.main_activity_spinner_item, R.id.textView1,
+				spinnerValues));
+
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> adapter, View arg1,
+					int index, long arg3) {
+
+				MainActivityListHelper.bindListViewItems(a, data, index);
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+
+			}
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void runButtonLogic(Activity a) {
+		if (!Util.internetConnectionExists(a)) {
+			Toast.makeText(
+					a,
+					a.getString(R.string.no_active_internet_connection_please_check_your_settings),
+					Toast.LENGTH_LONG).show();
+
+			return;
+		}
+
+		// default not selected
+		if (!MainActivityHelper.getSpinnerSelectedValue(a).equals(
+				a.getString(R.string.spinner_default_selection))) {
+
+			ArrayList<UserProfile> data = new DbAdapter(a).loadAllProfiles();
+
+			// counter for rate-us-dialog
+			if (MainActivityHelper.rateUsDialog(a, 15, 50))
+				return;
+
+			int spinnerSelectedValueNr = MainActivityHelper
+					.getSpinnerSelectedIndex(a) - 1;
+
+			if (data == null || spinnerSelectedValueNr >= data.size()
+					|| spinnerSelectedValueNr < 0)
+				return;
+
+			// find keywords by name
+			ArrayList<Keyword> keywords = MainActivityHelper
+					.findKeywordsByName(data, spinnerSelectedValueNr);
+
+			if (keywords != null) {
+
+				// start download
+				new AsyncDownloader(a,
+						(ListView) a.findViewById(R.id.listview_result),
+						data.get(spinnerSelectedValueNr).url).execute(keywords);
+			}
+
+			else {
+				Toast.makeText(
+						a,
+						a.getString(R.string.profile_keywords_are_missing_how_s_thats_possible_),
+						Toast.LENGTH_SHORT).show();
+			}
+
+		} else {
+
+			Toast.makeText(a, a.getString(R.string.please_select_a_profile),
+					Toast.LENGTH_SHORT).show();
+
+		} // if empty selected
 	}
 
 }
---
+++ src/com/inc/im/serptracker/util/Premium.java
@@ -0,0 +1,105 @@
+package com.inc.im.serptracker.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.inc.im.serptracker.R;
+import com.inc.im.serptracker.VerifyWebView;
+import com.inc.im.serptracker.adapters.DbAdapter;
+import com.inc.im.serptracker.data.Keyword;
+import com.inc.im.serptracker.data.UserProfile;
+
+public class Premium {
+
+	public static void addPremiumOnClick(final Activity a,
+			final int spinnerSelectedItemIndex) {
+
+		ListView lv = (ListView) a.findViewById(R.id.listview_result);
+
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					final int arg2, long arg3) {
+
+				Context con = a.getBaseContext();
+
+				// -1 because first is default text
+				final UserProfile selectedUser = new DbAdapter(con)
+						.loadAllProfiles().get(spinnerSelectedItemIndex - 1);
+
+				final Keyword k = selectedUser.keywords.get(arg2);
+
+				// display dialog
+				final CharSequence[] items = { "View",
+						"ANCHOR: " + k.anchorText, "URL: " + k.url };
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(a);
+
+				builder.setTitle("Extra info for ranking");
+				builder.setItems(items, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int item) {
+
+						if (item == 0) {
+
+							// first reserved for verify ranking
+							try {
+
+								String userSearchEngine = PreferenceManager
+										.getDefaultSharedPreferences(a)
+										.getString("prefLocalize", "Google.com");
+
+								Toast.makeText(
+										a,
+										"www."
+												+ userSearchEngine
+												+ "/seach?q="
+												+ URLEncoder.encode(k.keyword,
+														"UTF-8"),
+										Toast.LENGTH_LONG).show();
+							} catch (UnsupportedEncodingException e) {
+								Log.e("MY", e.toString());
+								e.printStackTrace();
+							}
+
+							Intent i = new Intent(a, VerifyWebView.class);
+							i.putExtra("keyword", k.keyword);
+							i.putExtra("url", selectedUser.url);
+
+							a.startActivity(i);
+						} else if (item == 1) {
+
+							// second reserved for show ancho
+							Toast.makeText(a, k.anchorText, Toast.LENGTH_SHORT)
+									.show();
+
+						} else if (item == 2) {
+							// third reserved for visit url
+							a.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+									.parse(selectedUser.keywords.get(arg2).url)));
+						}
+
+					}
+				});
+				AlertDialog alert = builder.create();
+
+				alert.show();
+
+			}
+		});
+	}
+
+}
